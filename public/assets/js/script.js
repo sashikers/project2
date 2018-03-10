4e21aabaa99717b4f2dd722a49e3d1c4aa991a51
Barry Williams <barrywilliams84@gmail.com>
2018-03-10 11:12:53 -0800

    login button and event listener added

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,13 +1,4 @@
 //After the page loads...
 $(function(){
 
-	$.ajax('/api/test', {
-		type:"post",
-	}).done(function(data){
-		console.log('post response from server:', data)
-	}).fail(function(err){
-		alert(err);
-	});
-
-
 });
