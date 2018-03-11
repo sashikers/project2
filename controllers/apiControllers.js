039b4b75b55ea9dcbbacc8613e419ed69b1d0b20
Clayton <claytonhthompson@gmail.com>
2018-03-11 13:26:29 -0700

    finished implementing product CRUD on editor page

--- controllers/apiControllers.js
+++ controllers/apiControllers.js
@@ -4,9 +4,9 @@ const models = require('../models/index');
 
 module.exports = (app) => {
 
+	//get all products
 	app.get('/api/product/list', (req,res) => {
-
-		let query = `SELECT * FROM product ORDER BY createdAt DESC`;
+		let query = `SELECT * FROM product ORDER BY createdAt ASC`;
 		database.query(query,(err, rows) => {
 			if(err) return res.status(404).json(err);
 			res.status(200).json(rows);
@@ -14,9 +14,8 @@ module.exports = (app) => {
 
 	});
 
+	//create product
 	app.post('/admin/api/editor/', (req, res) => {
-
-		console.log("Here is the req.body", req.body.id);
 
 		let query = `INSERT INTO product SET ?`;
 		database.query(query, [req.body], (err,rows) => {
@@ -30,17 +29,28 @@ module.exports = (app) => {
 
 	});
 
+	//update product
 	app.put('/admin/api/editor/:id', (req, res) => {
 		let id = req.params.id;
-		console.log('Updating item! -- ', id);
 		let query = `UPDATE product SET ? WHERE id = ?`;
 		database.query(query, [req.body, id], (err,rows) => {
 			if(err) return res.status(404).json(err);
 			res.status(200).json(rows);
 		});
-
 	});
 
+	//delete product
+	app.delete('/admin/api/editor/:id', (req, res) => {
+		let id = req.params.id;
+		models.Product.destroy({where:[{id}]}).then((affectedRows) => {
+			if(affectedRows === 0) res.sendStatus(404);
+			res.sendStatus(200);
+		}).catch((err) => {
+			res.status(404).json(err);
+		})
+	});
+
+	//get product
 	app.get('/admin/api/product/:id', (req, res) => {
 		let id = req.params.id;
 		models.Product.findOne({where:[{id}]}).then((product) => {
