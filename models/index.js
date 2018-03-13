1612d87a0d6bf61c0518eb1de79a63f91644fef2
Clayton <claytonhthompson@gmail.com>
2018-03-12 21:04:32 -0700

    updated production config for heroku

--- models/index.js
+++ models/index.js
@@ -13,6 +13,7 @@ config.define = {
   freezeTableName: true,
 };
 
+console.log("***************", config.use_env_variable);
 
 if (config.use_env_variable) {
   var sequelize = new Sequelize(process.env[config.use_env_variable], config);
